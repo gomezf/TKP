171e5417348667435a7d14e9ad314e0886c99998
Francisco Gomez <fgomez@slb.com>
2015-01-11 13:33:25 +0800

    SimpleSquare-Seb

--- TeachingKidsProgramming/01.SimpleSquare/SimpleSquare.cs
+++ TeachingKidsProgramming/01.SimpleSquare/SimpleSquare.cs
@@ -7,23 +7,25 @@ namespace SimpleSquare
 		public static void Main()
 		{
 			//	Show the tortoise --#1
-            Tortoise.Show();
-
-                            
+              Tortoise.Show();
             //Tortoise MyTestTortoise;
 
 			//  Make the tortoise move as fast as possible  --#6
-
+              Tortoise.SetSpeed(8);
 			//  Do the following 4 times --#5.1
 
-			//     Change the color of the line the tortoise draws to blue --#4
+              for (int i = 0; i < 4; i++)
+              {
 
-			//     Move the tortoise 50 pixels --#2
 
-			//     Turn the tortoise 90 degrees to the right --#3
-
-			//  Repeat --#5.2
-
+                  //     Change the color of the line the tortoise draws to blue --#4
+                  Tortoise.SetPenColor("blue");
+                  //     Move the tortoise 50 pixels --#2
+                  Tortoise.Move(50);
+                  //     Turn the tortoise 90 degrees to the right --#3
+                  Tortoise.Turn(90);
+                  //  Repeat --#5.2
+              }
 		}
 	}
 }
